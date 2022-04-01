e2373abb14fe41d2da16e34ccd73031fe76dd360
Henry Letton <50699087+henryletton@users.noreply.github.com>
2022-04-01 14:48:17 +0100

    Fix coded quality

--- python_fns.py
+++ python_fns.py
@@ -5,7 +5,8 @@ def example_fn(num_1, num_2):
     '''Example adding function'''
     num_add = num_1 + num_2
     return num_add
+
+
 def example_fn2():
-    x = 1
-    
+    '''Prints a greeting'''
     return "Hello world!"
